fce1b1e4272ed68dd6e343981d2b1f6112e86c4e
yasaichi <yasaichi@users.noreply.github.com>
2017-09-02 17:45:12 +0900

    Implement spec of ActionView::ConsistentFallback::TemplateRendering

---
+++ spec/lib/actionview/consistent_fallback/template_rendering_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ActionView::ConsistentFallback::TemplateRendering, type: :controller do
+  render_views
+
+  controller(ApplicationController) do
+    prepend_view_path File.expand_path("../../../../fixtures/views", __FILE__)
+
+    before_action do
+      request.variant = params[:variant].to_sym if params[:variant].present?
+    end
+
+    def index
+      # This action has one variant template (.html+phone.erb) defined in the file system.
+    end
+  end
+
+  subject { response.body.split }
+
+  before do
+    get :index, params: { variant: variant }
+  end
+
+  context "when a variant isn't specified" do
+    let(:variant) { nil }
+    it { is_expected.to eq %w(default_layout default_template default_partial) }
+  end
+
+  context "when there is a variant template corresponding to each request variant" do
+    let(:variant) { :phone }
+    it { is_expected.to eq %w(phone_layout phone_template phone_partial) }
+  end
+
+  context "when there is no variant template corresponding to each request variant" do
+    let(:variant) { :tablet }
+    it { is_expected.to eq %w(default_layout default_template default_partial) }
+  end
+end
