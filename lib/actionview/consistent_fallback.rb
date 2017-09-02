5747bd77b488d15dd4392dd08653a911ae785ac2
yasaichi <yasaichi@users.noreply.github.com>
2017-09-02 14:11:23 +0900

    Run `rubocop --auto-correct`

--- lib/actionview/consistent_fallback.rb
+++ lib/actionview/consistent_fallback.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Actionview
   module ConsistentFallback
     # Your code goes here...
--- lib/actionview/consistent_fallback/version.rb
+++ lib/actionview/consistent_fallback/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Actionview
   module ConsistentFallback
-    VERSION = '0.1.0'
+    VERSION = "0.1.0"
   end
 end
